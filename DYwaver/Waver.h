84aba48a75c7d33883972f7cfb7b73998087b2ba
阳眼的熊 <237505990@qq.com>
2017-12-11 15:16:36 +0800

    Initial commit

---
+++ DYwaver/Waver.h
@@ -0,0 +1,15 @@
+//
+//  Waver.h
+//  DYwaver
+//
+//  Created by huyangyang on 2017/12/11.
+//  Copyright © 2017年 HuYangYang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Waver : UIView
+@property (nonatomic, copy) void (^waverLevelCallback)(Waver * waver);
+
+-(instancetype)initWithFrame:(CGRect)frame andNumberOfWaves:(NSUInteger)numberOfWaves andWavesColors:(NSArray*)colorsAry andDecorativeWavesWidth:(CGFloat)decorativeWavesWidth;
+@end
